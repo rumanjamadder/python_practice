af5625485a33afc65872c3d1afe0db5f9b2edd1b
rumanjamadder <jamadderruman797@gmail.com>
2024-01-01 15:24:09 +0600

    question_added

--- kbc.py
+++ kbc.py
@@ -1,4 +1,23 @@
 print("KON BANEGA KORARPATI")
 print()
 
+# players name
 players = ["players name", "_________", "1. Harry", "2. Ruman", "3. Talha", "4. ALvee"]
+# print players
+for player in players:
+    print(player)
+
+    print("\n")
+# Questions and Answers:
+InventorOfPython = ["Question: InventorOfPython is:", "______________________________",
+                    "a. Guido Van Rossum()",
+                    "b. Marks Zukerbag()",
+                    "c. Benjamin Franklin()",
+                    "d. James Bond"]
+for invetor in InventorOfPython:
+    print(invetor)
+
+    print("\n")
+
+                    
+
